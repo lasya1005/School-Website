3c9e28ebff3935d618c4937a9f923c2a58bf6d20
lasya1005 <121902179+lasya1005@users.noreply.github.com>
2024-11-25 03:08:11 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Responsive Navbar
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector('.burger'); 
+    const navLinks = document.querySelector('.nav-links'); 
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('nav-active'); 
+        burger.classList.toggle('toggle'); 
+    });
+});
